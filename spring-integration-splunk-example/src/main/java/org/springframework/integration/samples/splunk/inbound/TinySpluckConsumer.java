d413513367e46853671248e23c153f32d6c0e5a0
fbalicchia <fbalicchia@gmail.com>
2015-12-07 15:13:53 +0100

    Minor change

--- spring-integration-splunk-example/src/main/java/org/springframework/integration/samples/splunk/inbound/TinySpluckConsumer.java
+++ spring-integration-splunk-example/src/main/java/org/springframework/integration/samples/splunk/inbound/TinySpluckConsumer.java
@@ -2,19 +2,14 @@ package org.springframework.integration.samples.splunk.inbound;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class TinySpluckConsumer {
 
-public class TinySpluckConsumer
-{
+	private static final String CONFIG = "si-splunk-consumer-context.xml";
 
-    private static final String CONFIG = "si-splunk-consumer-context.xml";
+	public static void main(final String args[]) {
+		final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG, TinySpluckConsumer.class);
+		ctx.start();
 
-
-    public static void main(final String args[])
-    {
-        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG, TinySpluckConsumer.class);
-        ctx.start();
-
-    }
-
+	}
 
 }
--- spring-integration-splunk-example/src/main/java/org/springframework/integration/samples/splunk/model/OrderSuspectEvent.java
+++ spring-integration-splunk-example/src/main/java/org/springframework/integration/samples/splunk/model/OrderSuspectEvent.java
@@ -2,40 +2,34 @@ package org.springframework.integration.samples.splunk.model;
 
 import org.springframework.integration.splunk.event.SplunkEvent;
 
-
 /**
  * @author fbalicchia
  * @version $Id: $
  */
-public class OrderSuspectEvent extends SplunkEvent
-{
+public class OrderSuspectEvent extends SplunkEvent {
 
-    private static final long serialVersionUID = 4203808769099452241L;
+	private static final long serialVersionUID = 4203808769099452241L;
 
-    public static String ORDER_NUMBER = "recipient";
+	public static String ORDER_NUMBER = "order_number";
 
-    public static String EAN_ITEM = "sender";
+	public static String EAN_ITEM = "ean";
 
-    public static String EMAIL_USER = "subject";
+	public static String EMAIL_USER = "email";
 
-    public OrderSuspectEvent()
-    {
-        super();
-    }
+	public OrderSuspectEvent() {
+		super();
+	}
 
-    public void setOrderNumber(String orderNumber)
-    {
-        addPair(ORDER_NUMBER, orderNumber);
-    }
+	public void setOrderNumber(String orderNumber) {
+		addPair(ORDER_NUMBER, orderNumber);
+	}
 
-    public void setEan(String eanNumber)
-    {
-        addPair(EAN_ITEM, eanNumber);
-    }
+	public void setEan(String eanNumber) {
+		addPair(EAN_ITEM, eanNumber);
+	}
 
-    public void setEmailuser(String email)
-    {
-        addPair(EMAIL_USER, email);
-    }
+	public void setEmailuser(String email) {
+		addPair(EMAIL_USER, email);
+	}
 
 }
