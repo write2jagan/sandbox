95ca69d6e206cafe6cf4fd8e4d2e0a382cb71919
filippo balicchia <fbalicchia@gmail.com>
2017-01-11 07:47:14 +0100

    Update GelfSender.java
    never put constant in interface :-(

--- ya-gelf-logger/src/main/java/it/balyfix/gelf/logger/sender/GelfSender.java
+++ ya-gelf-logger/src/main/java/it/balyfix/gelf/logger/sender/GelfSender.java
@@ -3,8 +3,7 @@ package it.balyfix.gelf.logger.sender;
 import it.balyfix.gelf.logger.message.GelfMessage;
 
 public interface GelfSender {
-	public static final int DEFAULT_PORT = 12201;
-
+	
 	public GelfSenderResult sendMessage(GelfMessage message);
 	
 	public void close();
